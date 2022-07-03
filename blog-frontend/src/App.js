1bca6234b3802b3d436d2448b0eea7f99fcbe0cc
Jiuk Choi <jiukchoi@algotradingsysteminc.com>
2022-07-02 20:33:38 -0500

    update app.js
    a features added

--- blog-frontend/src/App.js
+++ blog-frontend/src/App.js
@@ -4,11 +4,16 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 function App() {
-  return (
-    <div className="App">
-        
-    </div>
-  );
+    var data = "specialComment";
+    let post = '강남 우동 맛집';
+    return (
+        <div className="App">
+            <div className="black-nav">
+                <h4>블로그임</h4>
+                <h4 style={{ padding: '0px 20px' }}>{post}</h4>
+            </div>
+        </div>
+    );
 }
 
 export default App;
