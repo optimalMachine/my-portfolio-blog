8a23633606c1f634c3255caa3dacab7e2868fc0f
Jiuk Choi <jiukchoi@algotradingsysteminc.com>
2022-07-07 17:58:57 -0500

    Update App.js
    likeit button small bug fixed.

--- blog-frontend/src/App.js
+++ blog-frontend/src/App.js
@@ -8,6 +8,8 @@ function App() {
 
     let [title, setTitle]= useState(['남자 코트 추천','여자 코트 추천','강남 우동 맛집']);
     let [likeit, setLikeit] = useState(0);
+    let [modalOn,setModalOn] = useState('false');
+
     return (
         <div className="App">
             <Navbar bg="dark" variant="dark">
@@ -16,19 +18,15 @@ function App() {
                 </Container>
             </Navbar>
             <div className="list">
-                <h4>{title[0]} <span onClick={()=>{
+                <h4 onClick={()=>{
+                    setModalOn(!modalOn);
+                }}>{title[0]} <span onClick={(event)=>{
+                    event.stopPropagation();
                     setLikeit(likeit+1);
                 }}> <i class="bi bi-hand-thumbs-up-fill"></i></span>{likeit}</h4>
                 <p>2월 17일 발행</p>
             </div>
-            <div className="list">
-                <h4>여자 코트 추천</h4>
-                <p>2월 17일 발행</p>
-            </div>
-            <div className="list">
-                <h4>강남 우동 맛집</h4>
-                <p>2월 17일 발행</p>
-            </div>
+
             <div className="buttons">
                 {/* 남자를 여자로 바꾸는 버튼 */}
                 <div className="buttons-button-01">
@@ -47,8 +45,17 @@ function App() {
                     }}>제목정렬버튼</button>
                 </div>
             </div>
+            { modalOn == true ? <ModalWindow /> : null }
+        </div>
+    );
+}
 
-
+function ModalWindow(){
+    return(
+        <div className="modal-window">
+            <h4>제목</h4>
+            <p>날짜</p>
+            <p>상세내용</p>
         </div>
     );
 }
