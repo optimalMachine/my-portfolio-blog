da1ae6ba406e7538e1b064228b2ea3afb2a5ab88
Jiuk Choi <jiukchoi@algotradingsysteminc.com>
2022-07-07 16:49:10 -0500

    Update App.js
    Adds more buttons

--- blog-frontend/src/App.js
+++ blog-frontend/src/App.js
@@ -7,7 +7,7 @@ import { Button, Container, Navbar, Nav } from 'react-bootstrap';
 function App() {
 
     let [title, setTitle]= useState(['남자 코트 추천','여자 코트 추천','강남 우동 맛집']);
-    
+    let [likeit, setLikeit] = useState(0);
     return (
         <div className="App">
             <Navbar bg="dark" variant="dark">
@@ -16,7 +16,9 @@ function App() {
                 </Container>
             </Navbar>
             <div className="list">
-                <h4>남자 코트 추천</h4>
+                <h4>{title[0]} <span onClick={()=>{
+                    setLikeit(likeit+1);
+                }}> <i class="bi bi-hand-thumbs-up-fill"></i></span>{likeit}</h4>
                 <p>2월 17일 발행</p>
             </div>
             <div className="list">
@@ -27,6 +29,26 @@ function App() {
                 <h4>강남 우동 맛집</h4>
                 <p>2월 17일 발행</p>
             </div>
+            <div className="buttons">
+                {/* 남자를 여자로 바꾸는 버튼 */}
+                <div className="buttons-button-01">
+                    <button onClick={()=>{
+                        let temp = [...title];
+                        temp[0] = '여자 코트 추천';
+                        setTitle(temp);
+                    }}>여자로 바꾸기</button>
+                </div>
+                {/* 타이틀 배열 정렬 버튼 */}
+                <div className="buttons-button-02">
+                    <button onClick={()=>{
+                        let temp = [...title];
+                        temp = temp.sort();
+                        setTitle(temp);
+                    }}>제목정렬버튼</button>
+                </div>
+            </div>
+
+
         </div>
     );
 }
